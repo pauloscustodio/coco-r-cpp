21d5d9f77b8346e36774088b1e06f9b6ef297981
pauloscustodio <pauloscustodio@gmail.com>
2015-09-20 20:14:44 +0100

    Input file to .tas program is now a command line argument
    Was hard-coded.

--- Taste/Taste.cpp
+++ Taste/Taste.cpp
@@ -11,7 +11,7 @@ using namespace Taste;
 
 int main (int argc, char *argv[]) {
 
-	if (argc == 2) {
+	if (argc == 3) {
 		wchar_t *fileName = coco_string_create(argv[1]);
 		Taste::Scanner *scanner = new Taste::Scanner(fileName);
 		Taste::Parser *parser = new Taste::Parser(scanner);
@@ -20,7 +20,7 @@ int main (int argc, char *argv[]) {
 		parser->Parse();
 		if (parser->errors->count == 0) {
 			parser->gen->Decode();
-			parser->gen->Interpret("Taste.IN");
+			parser->gen->Interpret(argv[2]);
 		}
 
 		coco_string_delete(fileName);
